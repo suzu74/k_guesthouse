89aa014e90111bf31e7fe89f7b950c8d4e4b5331
鈴木友章 <tomosuzu74@gmail.com>
2019-06-18 19:53:44 +0900

    Create full_title medhod at application helper

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 Rails.application.routes.draw do
-  get '/', to: 'basic_pages#home'
+  root to: 'basic_pages#home'
   get 'basic_pages/home'
   get '/about', to: 'basic_pages#about'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
