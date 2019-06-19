b5953e0827e3682b2fdefe71d831af432b4f3624
鈴木友章 <tomosuzu74@gmail.com>
2019-06-19 20:24:17 +0900

    Create basic login page including SessionsController

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,12 @@
 Rails.application.routes.draw do
+  get 'sessions/new'
   root to: 'basic_pages#home'
   get '/home',  to: 'basic_pages#home'
   get '/about', to: 'basic_pages#about'
   get '/signup', to: 'users#new'
   post '/signup', to: 'users#create'
+  get    '/login',   to: 'sessions#new'
+  post   '/login',   to: 'sessions#create'
+  delete '/logout',  to: 'sessions#destroy'
   resources :users
 end
