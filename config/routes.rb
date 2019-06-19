2ed4583b6b8ccabcc566c8df0ee32a5c01097177
鈴木友章 <tomosuzu74@gmail.com>
2019-06-19 11:19:25 +0900

    Make user register page

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,8 @@
 Rails.application.routes.draw do
-  get 'users/new'
   root to: 'basic_pages#home'
   get '/home',  to: 'basic_pages#home'
   get '/about', to: 'basic_pages#about'
   get '/signup', to: 'users#new'
+  post '/signup', to: 'users#create'
+  resources :users
 end
