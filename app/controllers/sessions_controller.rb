3d74daa5d9780726c1552ac1f282036e0355c8de
鈴木友章 <tomosuzu74@gmail.com>
2019-06-20 22:02:29 +0900

    Make Active Storage image function

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -8,6 +8,7 @@ class SessionsController < ApplicationController
     if user && user.authenticate(params[:session][:password])
       log_in user
       params[:session][:remember_me] == '1' ? remember(user) : forget(user)
+      flash[:success] = 'ログインに成功しました！'
       redirect_back_or user
     else
       flash.now[:danger] = 'メールアドレス/パスワードの間違いがあります。'
