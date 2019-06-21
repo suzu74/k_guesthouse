ff5b9bfaa0d729faec06989f01d758eaf720873f
鈴木友章 <tomosuzu74@gmail.com>
2019-06-21 09:55:06 +0900

    Make profile comment

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -49,7 +49,7 @@ class UsersController < ApplicationController
   private
 
     def user_params
-      params.require(:user).permit(:name, :email, :image, :password,
+      params.require(:user).permit(:name, :email, :comment, :image, :password,
                                    :password_confirmation)
     end
 
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_06_20_101059) do
+ActiveRecord::Schema.define(version: 2019_06_20_132010) do
 
   create_table "active_storage_attachments", force: :cascade do |t|
     t.string "name", null: false
@@ -41,6 +41,7 @@ ActiveRecord::Schema.define(version: 2019_06_20_101059) do
     t.string "password_digest"
     t.string "remember_digest"
     t.boolean "admin", default: false
+    t.string "comment"
     t.index ["email"], name: "index_users_on_email", unique: true
   end
 
