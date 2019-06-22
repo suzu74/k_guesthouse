489067bc12d0d28b7b88e3cbbbbe29303db42e3b
鈴木友章 <tomosuzu74@gmail.com>
2019-06-22 09:29:31 +0900

    Add feature spec

--- spec/features/basic_pages_spec.rb
+++ spec/features/basic_pages_spec.rb
@@ -2,15 +2,21 @@ require 'rails_helper'
 
 RSpec.feature 'BasicPages', type: :feature do
 
-  scenario 'カテゴリーページから商品詳細ページへ飛び、商品名が表示されている' do
+  scenario 'topページに訪れてtitleが正しくaboutページへのリンクがある' do
     visit root_path
     expect(page).to have_title "K-Guesthouse"
     expect(page).to have_link 'About', href: about_path
   end
 
-  scenario 'カテゴリーページから商品詳細ページへ飛び、商品名が表示されている' do
+  scenario 'Aboutページに訪れてtitleが正しくtopページへのリンクがある' do
     visit about_path
     expect(page).to have_title "About | K-Guesthouse"
     expect(page).to have_link 'Home', href: root_path
+  end
+
+  scenario 'topページからsignupページへ行ける' do
+    visit root_path
+    click_on "Sign up now"
+    expect(current_path).to eq signup_path
   end
 end
